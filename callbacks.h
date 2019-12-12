94cd61e48267f4dbcaeb25939ece6f73d004adfb
sami571 <sami.belmabrouk1@esprit.tn>
2019-12-12 00:46:27 -0500

    ajout d'un fichier callbacks.h

---
+++ callbacks.h
@@ -0,0 +1,56 @@
+#include <gtk/gtk.h>
+
+
+void
+on_button1_clicked                     (GtkButton       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button3_clicked                     (GtkButton       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button6_clicked                     (GtkButton       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button8_clicked                     (GtkButton       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button10_clicked                    (GtkButton       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button11_clicked                    (GtkButton       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button5_clicked                     (GtkButton       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button14_clicked                    (GtkButton       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button9_clicked                     (GtkButton       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_treeview1_row_activated             (GtkTreeView     *treeview,
+                                        GtkTreePath     *path,
+                                        GtkTreeViewColumn *column,
+                                        gpointer         user_data);
+
+void
+on_button7_clicked                     (GtkButton       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button7_clicked                     (GtkButton       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button8_clicked                     (GtkButton       *objet_graphique,
+                                        gpointer         user_data);
